1d554a321fa9848358f31163016b2c4e569b032a
Eduardo A. Farías <edufarias@gmail.com>
2021-09-28 22:14:56 -0300

    Activitis iniciales.

---
+++ app/src/main/java/cl/grupost/eval1androidgrupal/CargarSaldo.java
@@ -0,0 +1,14 @@
+package cl.grupost.eval1androidgrupal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CargarSaldo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cargar_saldo);
+    }
+}
